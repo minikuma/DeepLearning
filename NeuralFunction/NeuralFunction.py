b8e99357bc3114e2139e428fff1fb1c679be0ff0
전지훈(JI - HOON JEON)/Platform사업팀/SK <07762@skcc.com>
2017-11-30 16:38:24 +0900

    ver1.4

---
+++ NeuralFunction/NeuralFunction.py
@@ -0,0 +1,15 @@
+################################################################
+#  Perceptron - 인공신경망모델링 계단함수
+#               임계값을 경계로 출력이 변함 (0 -> 1)
+################################################################
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+x = np.arange(-5.0, 5.0, 0.1)   # -5.0 ~ 5.0 0.1 간격
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
